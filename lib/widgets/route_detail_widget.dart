34a1ac71b22b785111e164ae5834f8cdfbfe87c0
Federico Viceconti <viceconti.federico@gmail.com>
2021-03-29 14:59:33 +0200

    trip detail starting to making screen

--- lib/widgets/route_detail_widget.dart
+++ lib/widgets/route_detail_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:journey_demo/common/text/bold_text.dart';
 import 'package:journey_demo/common/widget/base_widget.dart';
 import 'package:journey_demo/notifier/route_detail_notifier.dart';
 import 'package:provider/provider.dart';
@@ -19,11 +20,29 @@ class _RouteDetailWidgetState extends State<RouteDetailWidget> {
 
   _buildBody() {
     final notifier = Provider.of<RouteDetailNotifier>(context, listen: false);
-    return GestureDetector(
-      onTap: () => notifier.pop(),
-      child: Icon(
-        Icons.arrow_back_ios,
-        color: Colors.orange,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () => notifier.pop(),
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.deepOrange,
+              ),
+            ),
+            SizedBox(height: 16),
+            BoldText(
+              "Trip detail",
+              fontSize: 32,
+              color: Colors.deepOrange,
+            ),
+            SizedBox(height: 16),
+            
+          ],
+        ),
       ),
     );
   }
